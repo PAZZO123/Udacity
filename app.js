b2ab125e000ec408918e538f1e35df50d4ef3576
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-11 12:52:02 +0200

    'J11'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('click',function() {
    // console.log('clicked')
    const heading=document.querySelector('h1')
    heading.style.color='purple'
-   heading.style.backgroundColor='red'
+   heading.style.backgroundColor='green'
    console.log(heading.style.color)
     
 })
