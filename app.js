7dba452d2dcd2306ffd309f5a8556861f1e1b28a
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-13 14:14:18 +0200

    'J12'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('keypress',function() {
    // // console.log('clicked')
     const heading=document.querySelector('h1')
    // heading.style.color='purple'
-   // heading.style.backgroundColor='blue'
+    heading.style.backgroundColor='blue'
    console.log(heading.style.backgroundColor)
    console.log('removing First Child')
    document.querySelector('#contain-all').firstElementChild.remove();
